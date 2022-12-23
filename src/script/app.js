5c27f4ceca2db70a80bee80979c5bfd8767bbd99
ramadanriz <zerorider96@gmail.com>
2022-12-23 09:18:33 +0700

    initial commit

---
+++ src/script/app.js
@@ -0,0 +1,6 @@
+import "regenerator-runtime";
+import "../styles/style.css";
+import "./component/app-bar.js";
+import "./component/search-bar.js";
+import main from "./view/main.js";
+document.addEventListener("DOMContentLoaded", main);
