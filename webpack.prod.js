03936161f48d61209325560b4069e67ff180f48a
ramadanriz <zerorider96@gmail.com>
2022-12-23 09:24:57 +0700

    fixing webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,7 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
     mode: "production",
-    module = {
+    module: {
         rules: [
             {
                 test: /\.js$/,
